4b092331049f032421d48f90089d9ac90506fef7
jberezow <jberez@live.ca>
2022-06-04 22:14:46 +0200

    Buggy PM handling

--- main.py
+++ main.py
@@ -47,6 +47,33 @@ async def convert_timezone(a, b, time):
     
     return str(new_hour) + ":" + minute
 
+async def convert_from_pm(time):
+    hour, minute = time.split(":")
+    hour = int(hour)
+    new_hour = hour + 12
+    return str(new_hour) + ":" + minute
+
+async def convert_to_pm(time):
+    hour, minute = time.split(":")
+    hour = int(hour)
+    if hour < 12:
+        return str(hour) + ":" + minute + "AM"
+    elif hour == 12:
+        return str(hour) + ":" + minute + "PM"
+    else:
+        new_hour = hour % 12
+        return str(new_hour) + ":" + minute + "PM"
+
+async def handle_pm(time):
+    raw_time_match = re.match("\d*:\d*", time)
+    raw_time = time[raw_time_match.start():raw_time_match.end()]
+    pm_match = re.findall("(pm)|(PM)",time)
+    if len(pm_match) == 0:
+        return raw_time
+    else:
+        new_time = await convert_from_pm(raw_time)
+        return new_time
+
 @tz.event
 async def on_message(message):
 
@@ -54,18 +81,20 @@ async def on_message(message):
     if message.author == tz.user:
         return
 
-    message_times = re.findall("\d*:\d*", message.content)
+    message_times = re.findall(r"^\d{1,2}:\d{1,2}$|^\d{1,2}:\d{1,2} pm$|^\d{1,2}:\d{1,2} PM$|^\d{1,2}:\d{1,2}pm$|^\d{1,2}:\d{1,2}PM$", message.content)
 
     if message_times:
         for time in message_times:
             timezone = await get_user_timezone(message.author.name)
-            response = [f"```{message.author.name} indicated a time: **{time} {timezone}**\n\nHere is the time in other timezones:\n--------------------------------------"]
+            time = await handle_pm(time)
+            time_pm = await convert_to_pm(time)
+            response = [f"```{message.author.name} indicated a time: **{time} {timezone} ({time_pm})**\n\nHere is the time in other timezones:\n----------------------------------------------"]
             for conversion in TIMEZONES.keys():
                 converted = await convert_timezone(timezone,conversion,time)
                 target_tz = LONG_TIMEZONES[conversion]
                 dif = 25 - len(target_tz)
-                print(dif)
-                response.append(f"\n{(target_tz+':'+''.join([' ' for _ in range(dif)]))}{converted :>10}")
+                converted_pm = await convert_to_pm(converted)
+                response.append(f"\n{(target_tz+':'+''.join([' ' for _ in range(dif)]))}{converted :>10}{converted_pm :>10}")
             response.append("```")
             response = "".join(response)
             await message.channel.send(response)
